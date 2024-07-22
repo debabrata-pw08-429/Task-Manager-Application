3f92b85543e1b5b060d261ce4ff9273d4e697e01
debabrata-pw08-429 <chintuthecoder@gmail.com>
2024-07-22 14:46:19 +0530

    Updated the server dependencies

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const passport = require("passport");
-const session = require("express-session");
 const cors = require("cors");
 
 // Load environment variables from .env file
